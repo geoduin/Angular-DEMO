5180ff62ce5c90a1fc2e3847b8476b0d4e4263e2
xin <xin20wang@outlook.com>
2022-11-11 14:41:16 +0100

    Made homework

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,20 @@ import { AColumnsComponent } from './features/component-a/a-columns.component';
 import { ADetailsComponent } from './features/component-a/a-details/a-details.component';
 import { AListComponent } from './features/component-a/a-list/a-list.component';
 import { BListComponent } from './features/component-b/b-list/b-list.component';
+import { UserDetailComponent } from './features/user/user-detail/user-detail.component';
+import { UserEditComponent } from './features/user/user-edit/user-edit.component';
+import { UserListComponent } from './features/user/user-list/user-list.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'component-a', pathMatch: "full"},
   {path: 'component-a', component: AListComponent},
   {path: 'component-b', component: BListComponent},
+  {path: 'user', component: UserListComponent, children:[
+    {path: 'userAdd', component: UserEditComponent},
+  ]},
+  {path: 'UserAdd' , component: UserEditComponent}
+  ,
+  {path: 'user/:id', component: UserDetailComponent},
   {path: 'component-a/:userId',pathMatch: "full", component: ADetailsComponent},
   {path: 'a-columns', component: AColumnsComponent, children: [
     {path: ":userId", component: ADetailsComponent}
